4ddbac9cc711b17cc2cc39265c5c3dc0abe25e44
ian <ian@ian-virtual-machine>
2014-09-09 23:59:19 +1000

    9th Sep Final with comments and a new index page with table styling and bugs fixed All features done

--- web/WEB-INF/sqlFile.sql
+++ web/WEB-INF/sqlFile.sql
@@ -1,3 +1,4 @@
+/*drop and create account related tables*/
 drop view jdbcrealm_user;
 drop view jdbcrealm_group;
 drop table account;
@@ -23,87 +24,11 @@ FROM AIP.ACCOUNT;
 select * from JDBCREALM_GROUP;
 select * from JDBCREALM_user;
 
-/*
-create view jdbcrealm_user (username, password) as
-select username, password
-from account;
-
-create view jdbcrealm_group (username, groupname) as
-select username, 'Users'
-from account;
-*/
-/*
-DROP TABLE AIP.PILOGTRAININGPROPERTY;
-DROP TABLE AIP.JOYFLIGHTPROPERTY;
-DROP TABLE AIP.ACTIVITY;
-DROP TABLE AIP.ACTIVITYTYPE;
-DROP TABLE AIP.AIRCRAFT;
-DROP TABLE AIP.ACTIVITYPROVIDER;
-
-
-CREATE TABLE AIP.ACTIVITYTYPE (
-    ID          INTEGER NOT NULL 
-                PRIMARY KEY GENERATED ALWAYS AS IDENTITY 
-                (START WITH 1, INCREMENT BY 1),
-    TYPENAME    VARCHAR(255) NOT NULL);
-
-
-CREATE TABLE AIP.AIRCRAFT (
-    ID          INTEGER NOT NULL 
-                PRIMARY KEY GENERATED ALWAYS AS IDENTITY 
-                (START WITH 1, INCREMENT BY 1),
-    MODEL    VARCHAR(255) NOT NULL);
-
-
-CREATE TABLE AIP.ACTIVITYPROVIDER (
-    ID          INTEGER NOT NULL 
-                PRIMARY KEY GENERATED ALWAYS AS IDENTITY 
-                (START WITH 1, INCREMENT BY 1),
-    NAME    VARCHAR(255) NOT NULL);
-
-
-CREATE table AIP.ACTIVITY (
-    ID          INTEGER NOT NULL 
-                PRIMARY KEY GENERATED ALWAYS AS IDENTITY 
-                (START WITH 1, INCREMENT BY 1),
-    TITLE       VARCHAR(255)NOT NULL, 
-    TYPEID      INTEGER NOT NULL,
-    PROVIDERID      INTEGER NOT NULL,
-    AIRCRAFTID      INTEGER NOT NULL,
-    ACTIVITYSTATE   VARCHAR(255) NOT NULL,
-    ACTIVITYDESC    VARCHAR(255) NOT NULL,
-    FOREIGN KEY (TYPEID)
-        REFERENCES AIP.ACTIVITYTYPE (ID),
-    FOREIGN KEY (PROVIDERID)
-        REFERENCES AIP.ACTIVITYPROVIDER (ID),
-    FOREIGN KEY (AIRCRAFTID)
-        REFERENCES AIP.AIRCRAFT(ID)
-);
-
-
-CREATE TABLE AIP.PILOGTRAININGPROPERTY (
-    ACTIVITYID  INTEGER NOT NULL 
-                PRIMARY KEY GENERATED ALWAYS AS IDENTITY 
-                (START WITH 1, INCREMENT BY 1),
-    CERTIFICATE    VARCHAR(255)NOT NULL,
-    DURATION    VARCHAR(255)NOT NULL,
-    FOREIGN KEY (ACTIVITYID)
-        REFERENCES AIP.ACTIVITY(ID));
-
-CREATE TABLE AIP.JOYFLIGHTPROPERTY (
-    ACTIVITYID  INTEGER NOT NULL 
-                PRIMARY KEY GENERATED ALWAYS AS IDENTITY 
-                (START WITH 1, INCREMENT BY 1),
-    CAPACITY    INTEGER NOT NULL,
-    FOREIGN KEY (ACTIVITYID)
-        REFERENCES AIP.ACTIVITY(ID));
-*/
+/*drop and create activity related tables*/
 DROP TABLE AIP.PILOTTRAINING;
 DROP TABLE AIP.JOYFLIGHT;
 DROP TABLE AIP.ACTIVITY;
-DROP TABLE AIP.ACTIVITYTYPE;
-DROP TABLE AIP.AIRCRAFT;
-DROP TABLE AIP.ACTIVITYPROVIDER;
+
 
 CREATE table AIP.ACTIVITY (
     ID          INTEGER NOT NULL 
@@ -140,7 +65,7 @@ CREATE TABLE AIP.JOYFLIGHT (
         REFERENCES AIP.ACTIVITY(ID) ON DELETE CASCADE);
 
 
-
+/* test scripts
 select activity.id,title,activitytype,provider,aircraft,activitystate,activitydesc,capacity 
 from aip.activity, aip.joyflight 
 where aip.activity.id = aip.joyflight.activityid;
@@ -154,10 +79,9 @@ insert into aip.joyflight (activityid,capacity) values (1,1);
 insert into aip.activity (title,activitytype,provider,aircraft,activitystate,activitydesc) values ('train1','PILOTTRAINING','provider2','f22','EXPIRED','activity desc');
 insert into aip.pilottraining (activityid,certificate,duration) values (2,'fighter pilot','3 months');
 
-
 insert into aip.activity (title,activitytype,provider,aircraft,activitystate,activitydesc) values ('testjoy1','JOYFLIGHT','provider1','y10','UPCOMING','activity desc');
 
 select activity.id,title,activitytype,provider,aircraft,activitystate,activitydesc,capacity 
 from aip.activity, aip.joyflight 
 where aip.activity.id = aip.joyflight.activityid
-and activity.id = 1;
+and activity.id = 1;*/
